b78d4aa49c455a1d7a02b3b9b1805c7eacaf05ab
ybon.dev <ybon.dev@gmail.com>
2025-03-02 16:11:23 +0800

    fixed contact form wherein it can send a message

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,121 +1,123 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import emailjs from "@emailjs/browser";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { FaEnvelope, FaMapMarker, FaPhoneAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 
 const Contact = () => {
   const [loading, setLoading] = useState(false);
-  const form = useRef();
+  const form = useRef(null);
 
-  const sendEmail = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
     setLoading(true);
-    emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_ID,
-        process.env.NEXT_PUBLIC_TEMPLATE,
-        form.current,
-        process.env.NEXT_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          setLoading(false);
-          console.log(result.text);
-        },
-        (error) => {
-          setLoading(false);
-          console.log(error.text);
+
+    if (!form.current) return;
+    const formData = new FormData(form.current);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbw6ntIdwk4MR501fyQ5B415aFwgqIWfSUm5r-dv6h_k3SfXDHMMRSVkuOVtQGIqHqGe/exec",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
         }
       );
+
+      if (response.ok) {
+        toast.success("Message sent successfully!");
+        form.current.reset();
+      } else {
+        toast.error("Failed to send message!");
+      }
+    } catch (error) {
+      alert("An error occurred while sending the message.");
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
-    <>
-      <div className="row gy-3">
-        <h1 className="header">
-          contact <span>me</span>
-        </h1>
-        <div className="col-md-6">
-          <div className="left_col">
-            <Image
-              src={"./assets/contact.svg"}
-              width={1000}
-              height={1000}
-              className="img-fluid"
-              priority
-              alt=""
-            />
-            <div className="details">
-              <div className="icon_details">
-                <FaMapMarker className="fa_icon" />
-                <Link
-                  href="https://maps.app.goo.gl/T9GSDH4PvBKXJ8PfA"
-                  target="_blank"
-                  className="link"
-                >
-                  Poblacion, Balete, Batangas
-                </Link>
-              </div>
-              <div className="icon_details">
-                <FaPhoneAlt className="fa_icon" />
-                <Link href={`tel:09309858393`} className="link">
-                  0930 985 8393
-                </Link>
-              </div>
-              <div className="icon_details">
-                <FaEnvelope className="fa_icon" />
-                <Link href={`mailto: ybon.dev@gmail.com`} className="link">
-                  ybon.dev@gmail.com
-                </Link>
-              </div>
+    <div className="row gy-3">
+      <h1 className="header">
+        Contact <span>Me</span>
+      </h1>
+      <div className="col-md-6">
+        <div className="left_col">
+          <Image
+            src="/assets/contact.svg"
+            width={1000}
+            height={1000}
+            className="img-fluid"
+            priority
+            alt="Contact Illustration"
+          />
+          <div className="details">
+            <div className="icon_details">
+              <FaMapMarker className="fa_icon" />
+              <Link
+                href="https://maps.app.goo.gl/T9GSDH4PvBKXJ8PfA"
+                target="_blank"
+                className="link"
+              >
+                Poblacion, Balete, Batangas
+              </Link>
+            </div>
+            <div className="icon_details">
+              <FaPhoneAlt className="fa_icon" />
+              <Link href="tel:09309858393" className="link">
+                0930 985 8393
+              </Link>
+            </div>
+            <div className="icon_details">
+              <FaEnvelope className="fa_icon" />
+              <Link href="mailto:ybon.dev@gmail.com" className="link">
+                ybon.dev@gmail.com
+              </Link>
             </div>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="right_col">
-            <form action="#" ref={form} onSubmit={sendEmail}>
-              <div>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name"
-                  name="user_name"
-                  required
-                />
-              </div>
-              <div className="mt-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email"
-                  name="user_email"
-                  required
-                />
-              </div>
-              <div className="mt-3">
-                <textarea
-                  cols="30"
-                  rows="10"
-                  className="form-control"
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
-              </div>
-              <div className="btn_send d-flex justify-content-end mt-3">
-                {loading ? (
-                  <button disabled>sending...</button>
-                ) : (
-                  <button>send message</button>
-                )}
-              </div>
-            </form>
-          </div>
+      </div>
+      <div className="col-md-6">
+        <div className="right_col">
+          <form ref={form} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              name="user_name"
+              required
+            />
+            <input
+              type="email"
+              className="form-control mt-3"
+              placeholder="Email"
+              name="user_email"
+              required
+            />
+            <textarea
+              cols="30"
+              rows="10"
+              className="form-control mt-3"
+              placeholder="Message"
+              name="message"
+              required
+            ></textarea>
+            <div className="btn_send d-flex justify-content-end mt-3">
+              <button type="submit" disabled={loading}>
+                {loading ? "Sending..." : "Send Message"}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import Nav from "@components/Nav";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "My Personal Portfolio | Ybon.dev",
@@ -13,6 +14,7 @@ const layout = ({ children }) => {
       <body className="body">
         <Nav />
         {children}
+        <Toaster />
       </body>
     </html>
   );
